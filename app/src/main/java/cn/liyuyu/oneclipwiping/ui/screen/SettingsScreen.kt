ae7d92e756f309271de422c3708e0120edab4f3f
呵呵小鱼 <liyu2140@gmail.com>
2022-04-02 17:32:27 +0800

    Update SettingsScreen.kt

--- app/src/main/java/cn/liyuyu/oneclipwiping/ui/screen/SettingsScreen.kt
+++ app/src/main/java/cn/liyuyu/oneclipwiping/ui/screen/SettingsScreen.kt
@@ -1,27 +1,38 @@
 package cn.liyuyu.oneclipwiping.ui.screen
 
-import android.content.Intent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Switch
-import androidx.compose.material.SwitchDefaults
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
-import androidx.lifecycle.Lifecycle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import cn.liyuyu.oneclipwiping.MainViewModel
-import cn.liyuyu.oneclipwiping.R
-import cn.liyuyu.oneclipwiping.service.GuardAccessibilityService
-import cn.liyuyu.oneclipwiping.service.GuardForegroundService
-import cn.liyuyu.oneclipwiping.utils.OnLifecycleEvent
-import cn.liyuyu.oneclipwiping.utils.StateUtil
 
 /**
  * Created by frank on 2022/3/21.
  */
 @Composable
-fun SettingsScreen(navController: NavController, viewModel: MainViewModel) {
+fun SettingsScreen(navController: NavController?, viewModel: MainViewModel?) {
+    Column {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Column {
+                Text("通知权限", fontSize = 20.sp)
+                Text("前台服务提高存活几率")
+            }
+        }
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Column {
+                Text("自启动",fontSize = 20.sp)
+                Text("自启动后台服务")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SettingsPreview() {
+    SettingsScreen(navController = null, viewModel = null)
 }
